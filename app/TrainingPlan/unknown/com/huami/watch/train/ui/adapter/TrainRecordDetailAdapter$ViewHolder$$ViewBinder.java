da8fe6dc4ef7a35fc3b370c2c14554b6e5515ca9
Neuer User <Neuer_User@xda_developers>
2017-01-17 22:37:39 +0100

    Initial commit of huanghe-1.2.5a decompiled selected apps

---
+++ app/TrainingPlan/unknown/com/huami/watch/train/ui/adapter/TrainRecordDetailAdapter$ViewHolder$$ViewBinder.java
@@ -0,0 +1,21 @@
+// Generated code from Butter Knife. Do not modify!
+package com.huami.watch.train.ui.adapter;
+
+import android.view.View;
+import butterknife.ButterKnife.Finder;
+import butterknife.ButterKnife.ViewBinder;
+
+public class TrainRecordDetailAdapter$ViewHolder$$ViewBinder<T extends com.huami.watch.train.ui.adapter.TrainRecordDetailAdapter.ViewHolder> implements ViewBinder<T> {
+  @Override public void bind(final Finder finder, final T target, Object source) {
+    View view;
+    view = finder.findRequiredView(source, 2131492893, "field 'weeklyNumber'");
+    target.weeklyNumber = finder.castView(view, 2131492893, "field 'weeklyNumber'");
+    view = finder.findRequiredView(source, 2131492892, "field 'dayTrainStatus'");
+    target.dayTrainStatus = finder.castView(view, 2131492892, "field 'dayTrainStatus'");
+  }
+
+  @Override public void unbind(T target) {
+    target.weeklyNumber = null;
+    target.dayTrainStatus = null;
+  }
+}
---
+++ app/TrainingPlan/unknown/com/huami/watch/train/ui/fragment/TrainWeeklyRecordDetailFragment$$ViewBinder.java
@@ -0,0 +1,27 @@
+// Generated code from Butter Knife. Do not modify!
+package com.huami.watch.train.ui.fragment;
+
+import android.view.View;
+import butterknife.ButterKnife.Finder;
+import butterknife.ButterKnife.ViewBinder;
+
+public class TrainWeeklyRecordDetailFragment$$ViewBinder<T extends com.huami.watch.train.ui.fragment.TrainWeeklyRecordDetailFragment> implements ViewBinder<T> {
+  @Override public void bind(final Finder finder, final T target, Object source) {
+    View view;
+    view = finder.findRequiredView(source, 2131492882, "field 'trainRecordTitle'");
+    target.trainRecordTitle = finder.castView(view, 2131492882, "field 'trainRecordTitle'");
+    view = finder.findRequiredView(source, 2131492956, "field 'currentWeekNumber'");
+    target.currentWeekNumber = finder.castView(view, 2131492956, "field 'currentWeekNumber'");
+    view = finder.findRequiredView(source, 2131492957, "field 'recyclearView'");
+    target.recyclearView = finder.castView(view, 2131492957, "field 'recyclearView'");
+    view = finder.findRequiredView(source, 2131492955, "field 'fullScrollView'");
+    target.fullScrollView = finder.castView(view, 2131492955, "field 'fullScrollView'");
+  }
+
+  @Override public void unbind(T target) {
+    target.trainRecordTitle = null;
+    target.currentWeekNumber = null;
+    target.recyclearView = null;
+    target.fullScrollView = null;
+  }
+}
